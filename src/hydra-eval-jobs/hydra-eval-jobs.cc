e85b9d32992524022ae9b7e67b09b14e1af7f839
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-12-23 10:43:51 +0100

    unrestrict eval

--- src/hydra-eval-jobs/hydra-eval-jobs.cc
+++ src/hydra-eval-jobs/hydra-eval-jobs.cc
@@ -325,10 +325,6 @@ int main(int argc, char * * argv)
         /* FIXME: The build hook in conjunction with import-from-derivation is causing "unexpected EOF" during eval */
         settings.builders = "";
 
-        /* Prevent access to paths outside of the Nix search path and
-           to the environment. */
-        evalSettings.restrictEval = true;
-
         /* When building a flake, use pure evaluation (no access to
            'getEnv', 'currentSystem' etc. */
         evalSettings.pureEval = pureEval;
